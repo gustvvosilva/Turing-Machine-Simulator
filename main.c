ba165d58759bc674eb089a1fec0e1cbbe8b4513d
gustvvosilva <gustavosilvadeoliveira02@gmail.com>
2024-09-10 01:24:25 -0300

    Funciona para uma palavra chumbada na fita. Falta deixar BEM MAIS dinâmico...

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
 
@@ -13,12 +14,12 @@ int main(){
     //     printf("%s", tudo);
     // }
 
-    char alfabeto[30];
-    int qtd_estados, qtd_transicoes;
+    char alfabeto[30], qtd_estados[2];
+    int qtd_transicoes;
 
-    fscanf(file, "%s %d %d", &alfabeto, &qtd_estados, &qtd_transicoes);
+    fscanf(file, "%s %s %d", &alfabeto, &qtd_estados, &qtd_transicoes);
 
-    printf("%s %d %d\n", alfabeto, qtd_estados, qtd_transicoes);
+    printf("%s %s %d\n", alfabeto, qtd_estados, qtd_transicoes);
 
     // char test1[1], test2[30], test3[30];
 
@@ -28,16 +29,11 @@ int main(){
 
     // printf("\naaa%sbbb%sccc%s", test1, test2, test3);
 
-    char lixo[2];
-    fgets(lixo, 2, file); // pegar a quebra de linha
-
     // char oi, oi2, oi3, oi4, oi5, oi6, oi7, oi8;
 
     // fscanf(file, "%c %c %c %c %c %c %c %c", &oi, &oi2, &oi3, &oi4, &oi5, &oi6, &oi7, &oi8);
 
     // printf("aaaa%c %c %c %c %c %c %c %c", oi, oi2, oi3, oi4, oi5, oi6, oi7, oi8);
-
-    char mapeamento[11][5];
 
     // for(int i = 0; i < qtd_transicoes; i++){
     //     for(int j = 0; j < 5; j++){
@@ -74,6 +70,11 @@ int main(){
 
     // printf("*%c", linha[4]);
 
+    char lixo[5];
+    fgets(lixo, 5, file); // pegar a quebra de linha
+
+    char mapeamento[11][5];
+
     char linha[12];
 
     for(int i = 0; i < qtd_transicoes; i++){
@@ -94,6 +95,46 @@ int main(){
         }
         printf("\n");
     }
+
+    char fita[] = "aaabbb-";
+    int resultado = 0, pos_cabeca = 0;
+    char estado[2] = "1";
+    bool flag;
+
+    do {
+
+        flag = false;
+
+        for(int i = 0; i < qtd_transicoes; i++){
+
+            if(mapeamento[i][0] == *estado && mapeamento[i][1] == fita[pos_cabeca]){
+
+                fita[pos_cabeca] = mapeamento[i][2];
+                *estado = mapeamento[i][4];
+
+                if(mapeamento[i][3] == 'D'){
+                    pos_cabeca++;
+                }
+                else {
+                    pos_cabeca--;
+                }
+
+                flag = true;
+                break;
+            }
+            
+        }
+
+        if(*estado == *qtd_estados){
+            resultado = 1;
+        }
+        else if(!flag){
+            resultado = 2;
+        }
+
+    } while(resultado == 0);
+
+    printf("final %s legal\n", fita);
 
     fclose(file);
     return 0;
